29da523894d4a48d1afc6cd119918f3eddadb5a8
Hariz Tarmizi <hrztrmz@gmail.com>
2022-05-31 21:30:43 +0800

    Added showing skins by operator with command !op_skin [operator]

--- Arkn.py
+++ Arkn.py
@@ -1,18 +1,26 @@
 # This example requires the 'message_content' intent.
 #Create a file called config.py and add a variable token as for this to work. token variable is filled with the discord bot token.
 
+from ast import excepthandler
+from discord.ext import commands #Used to find the skins
 import discord
-#import requests #Used for synchronous
+import requests #Used for synchronous
 from bs4 import BeautifulSoup
 import aiohttp
 import asyncio
 import config #File with the bot token innit
+import aiofiles
 
-
+client = commands.Bot(command_prefix='!')
 intents = discord.Intents.default()
+#client = discord.Client(intents=intents)
 
-client = discord.Client(intents=intents)
 
+async def fetch(site):
+    async with aiohttp.ClientSession() as session, \
+            session.get(site) as response:
+        return await response.text()
+    
 async def get_page(session, url): #Gets the content of the page
     async with session.get(url) as r:
         return await r.text()
@@ -41,14 +49,13 @@ def parse(results):
     return (list_url)
 
 def parse2(data2):
-    f = open("Arknights_Data2.txt", 'w')
+    f = open("Arknights_Data.txt", 'w')
     f.seek(0)
     f.writelines("Arknights OP Event Data\n")
     OP_tq_new = 0
     OP_tq_old = 0
     for num, html in enumerate(data2):
         dud = 0
-
         new_p = 0
         soup = BeautifulSoup(html, "html.parser")
         s = soup.find('div', id= 'page-title')
@@ -95,6 +102,114 @@ async def start(urls):
         data2 = await get_all(session, list_url)
         parse2(data2)
 
+async def skin_f(urls, message): #Is redundant, useful if only wants a single skin info, but going to put it at op_skin function. Can delete
+    base_url = "https://gamepress.gg"
+    infor = ""
+    async with aiohttp.ClientSession() as session:
+        data = await get_page(session, urls)
+        soup = BeautifulSoup(data, "html.parser")
+        
+        #FInds the Name of skin
+        s = soup.find('div', id= 'page-title')
+        title = s.find('h1')
+        print(title.text) #Send Discord msg
+        infor = infor + "Skin Name: " + title.text
+        
+        #Find the cost of skin
+        cost = soup.find('span', class_="material-quantity")
+        print("Originium Prime cost: " + cost.text[1:]) #Send Discord msg
+        infor = infor + "\nOriginium Prime cost: " + cost.text[1:]
+        
+        #Find the image of skin
+        img_div = soup.find('div', id= 'image-tab-1')
+        img_w_tag = img_div.find('img')
+        img_url = base_url + img_w_tag.attrs["src"]
+        print(img_url)
+        
+        #Find the operator name
+        artist_div = soup.find('div', class_="skin-artist skin-info-box")
+        model_design = artist_div.find_all('a')
+        print("Model: " + model_design[0].text)
+        infor = infor + "\nModel: " + model_design[0].text
+        #print(model_design.text[1]) #For the artist of the skin
+        
+        #Finds the series
+        series_div = soup.find('div', class_="skin-series skin-info-box")
+        series = series_div.find_all('a')
+        print("Series: " + series[0].text)
+        infor = infor + "\nSeries: " + series[0].text
+        
+        async with aiofiles.open('skin_search.jpg', 'wb') as f:
+            resp = await session.request(method="GET", url=img_url)
+            await f.write(await resp.read())
+            #Do a send file here
+            await message.channel.send(file=discord.File('skin_search.jpg'))
+            await message.channel.send(infor) #Sends the whole information about the skin
+
+async def skin_by_op(skin_list_url, filt, message):      
+    base_url = "https://gamepress.gg"
+    data = await fetch(skin_list_url)
+    soup = BeautifulSoup(data, "html.parser")
+    
+    #Getting the whole skins list
+    s = soup.find_all('div', class_ = 'views-field views-field-field-skin-image')
+    print("Use the in brackets keyword for !skin command\n")
+    await message.channel.send("Use the in brackets keyword for !skin command\n")
+    
+    for skin_entry in s:
+        skin_op_name = skin_entry.find_all('a')
+        op_name = skin_op_name[1].text
+        infor = ""
+        if filt in op_name.upper(): #Filter part
+            event_url = skin_op_name[0].get('href')
+            search_url = event_url.split('/')
+            img_div = skin_op_name[2].find('img')
+            img_url = base_url + img_div.attrs["src"]
+            
+            url = "https://gamepress.gg"
+            end_url = url + event_url
+            print(end_url)
+            skin_page = await fetch(end_url)
+            s = BeautifulSoup(skin_page, "html.parser")
+            
+            #Find the cost
+            cost = s.find('span', class_="material-quantity")
+            try:
+                print("Originium Prime cost: " + cost.text[1:]) #Send Discord msg
+            except:
+                print("Originium Prime cost: Free\n")
+            #Finds the series
+            series_div = s.find('div', class_="skin-series skin-info-box")
+            try:
+                series = series_div.find_all('a')
+                print("Series: " + series[0].text)
+            except:
+                print("Series: None")
+            
+            #Skin img search function
+            print(img_url)
+            async with aiohttp.ClientSession() as session:
+                async with aiofiles.open('skin_search.jpg', 'wb') as f:
+                    resp = await session.request(method="GET", url=img_url)
+                    await f.write(await resp.read())
+                    await message.channel.send(file=discord.File('skin_search.jpg'))
+            
+            print("message send succesfful")
+            infor = infor + "Skin name: " + skin_op_name[0].text + " (" + search_url[-1] + ")\n"
+            infor = infor + "Operator : " + skin_op_name[1].text + "\n"
+            try:
+                infor = infor + "Series: " + series[0].text + "\n"
+            except:
+                infor = infor + "Series: None"
+            try:
+                infor = infor + "Originium Prime cost: " + cost.text[1:] + "\n"
+            except:
+                infor = infor + "Originium Prime cost: Free" + "\n"
+                
+            await message.channel.send(infor)
+            print("Skin name: " + skin_op_name[0].text + " (" + search_url[-1] + ")")
+            print("Operator : " + skin_op_name[1].text + "\n")
+            
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -106,7 +221,7 @@ async def on_message(message):
         return
 
     if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
+        await message.channel.send(message.content)
 
     #if message.content.startswith('$OP'): # Synchornous stuff, probably should not use. But it works
     #    f = open("Arknights_Data.txt", 'w')
@@ -176,13 +291,36 @@ async def on_message(message):
     #    f.close()
     #    await message.channel.send(file=discord.File("Arknights_Data.txt"))
     
-    if message.content.startswith('$OP'): #Asynchornous version of getting the Originium Prime. Means can do other stuff while doing this function
-        urls = [
+    await client.process_commands(message)
+
+
+@client.command()
+async def skin(message, *,arg):
+    if not arg.isdigit():
+        url = "https://gamepress.gg/arknights/skin/"
+    else:
+        url = "https://gamepress.gg/arknights/node/"
+    new_arg = arg.replace(" ", "-")
+    end_url = url + new_arg
+    try:
+        await client.loop.create_task(skin_f(end_url, message))
+    except:
+        await message.channel.send("Skin not found")
+    
+@client.command()
+async def OP(message):
+    urls = [
         "https://gamepress.gg/arknights/other/cn-event-and-campaign-list",
         ]
-        await client.loop.create_task(start(urls))
-        print("It doing stuff")
-        await message.channel.send(file=discord.File("Arknights_Data.txt"))
-        print("Text is sent")
-        
+    await client.loop.create_task(start(urls))
+    print("It doing stuff")
+    await message.channel.send(file=discord.File("Arknights_Data.txt"))
+    print("Text is sent")
+
+@client.command()
+async def op_skin(message, *, arg):
+    skin_list_url = "https://gamepress.gg/arknights/gallery/arknights-skin-art-gallery"
+    arg = arg.upper()
+    await client.loop.create_task(skin_by_op(skin_list_url, arg, message))
+
 client.run(config.token)
